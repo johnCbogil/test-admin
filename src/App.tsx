f3a7bf8ee6f8857b089a9b3d8ca20489db342901
John Bogil <johncbogil@gmail.com>
2023-06-23 11:10:06 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import { dataProvider } from './dataProvider';
+import { UserList } from './users';
+import { authProvider } from './authProvider';
+
+export const App = () => (
+    <Admin authProvider={authProvider} dataProvider={dataProvider}>
+     <Resource name="users" list={UserList} />
+     <Resource name="albums" list={ListGuesser} />
+    </Admin>
+  );
